1c1c18a11f64acbc51f6cf4c9055ee9e6d2f84c4
TheZoq2 <frans.skarman@protonmail.com>
2020-05-16 21:59:05 +0200

    Start modifying DMA example to use actual SPI

--- examples/spi-dma.rs
+++ examples/spi-dma.rs
@@ -22,33 +22,36 @@ fn main() -> ! {
     // HAL structs
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
 
     // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
     // `clocks`
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
     // Acquire the GPIOA peripheral
-    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
 
     let pins = (
-        gpiob.pb13.into_alternate_push_pull(&mut gpiob.crh),
-        gpiob.pb14.into_floating_input(&mut gpiob.crh),
-        gpiob.pb15.into_alternate_push_pull(&mut gpiob.crh),
+        gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl),
+        gpioa.pa6.into_floating_input(&mut gpioa.crl),
+        gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl),
     );
 
     let spi_mode = Mode {
         polarity: Polarity::IdleLow,
         phase: Phase::CaptureOnFirstTransition,
     };
-    let spi = Spi::spi2(dp.SPI2, pins, spi_mode, 100.khz(), clocks, &mut rcc.apb1);
+    let spi = Spi::spi1(dp.SPI1, pins, &mut afio.mapr, spi_mode, 100.khz(), clocks, &mut rcc.apb2);
 
     // Set up the DMA device
     let dma = dp.DMA1.split(&mut rcc.ahb);
 
     // Connect the SPI device to the DMA
-    let spi_dma = spi.with_tx_dma(dma.5);
+    let spi_dma = spi.with_tx_dma(dma.3);
 
-    // Start a DMA transfer
+    // Start a DMA transfe
+    static mut _BUFFER: [u8;4] = [0;4];
+    static mut BUFFER: &'static mut [u8;4] = unsafe{&mut _BUFFER};
     let transfer = spi_dma.write(b"hello, world");
 
     // Wait for it to finnish. The transfer takes ownership over the SPI device
